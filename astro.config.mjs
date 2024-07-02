18f71f1fb1b1d91b4bbfbe5414877f5b81be6af9
Administrator <Administrator@LENOVO>
2024-07-02 20:39:21 +0200

    Additional SSR thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import db from "@astrojs/db";
-
 import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), db(), auth()]
+  integrations: [vue(), db(), auth()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
