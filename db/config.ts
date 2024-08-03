9e4131e67a43fa24509136a7c6448896247d36f9
Ayato <ayatosan.pro@gmail.com>
2024-08-03 21:15:49 +0200

    feat(db): add user database

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,16 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db'
+
+const User = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    pseudo: column.text({ optional: true }),
+    email: column.text(),
+    password: column.text(),
+  },
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
-});
+  tables: { User },
+})
+
